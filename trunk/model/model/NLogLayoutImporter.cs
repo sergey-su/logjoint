d54bc742b23c9fe705104e34534193772674d174
sergeys_cp <SND\sergeys_cp>
2012-01-13 04:01:25 -0800

    Import NLog format: WIP

--- trunk/model/model/NLogLayoutImporter.cs
+++ trunk/model/model/NLogLayoutImporter.cs
@@ -34,8 +34,12 @@ namespace LogJoint
 			Node layout;
 			using (var e = ParseLayoutString(layoutString).GetEnumerator())
 				layout = MakeLayoutNode(e, false);
+			var layoutNoAmbProps = ConvertAmbientPropertiesToNodes(layout);
+			var res = GetNodeRegexps(layoutNoAmbProps, new NodeRegexContext()).ToArray();
 
-			OutputFieldType mandatoryFields = OutputFieldType.DateTime;
+			var ret = res.Aggregate(new StringBuilder(), (sb, re) => sb.AppendFormat("{0} # {1}{2}", re.Regex, re.Comment ?? "", Environment.NewLine)).ToString();
+
+			OutputFieldType mandatoryFields = OutputFieldType.Date | OutputFieldType.Time;
 			if ((processedFields & mandatoryFields) != mandatoryFields)
 				throw new NLogImportException("Date and time were not found in the layout");
 		}
@@ -101,23 +105,29 @@ namespace LogJoint
 			RendererParam // Renderer param. Data is a param name. Children has 1 element that stores param value.
 		};
 		[DebuggerDisplay("{Type} {Data}")]
-		struct Node
+		class Node
 		{
 			public NodeType Type;
 			public string Data;
 			public List<Node> Children;
+			public Node(NodeType t, string data, params Node[] children)
+			{
+				Type = t;
+				Data = data;
+				Children = new List<Node>(children);
+			}
 		};
 
 		static Node MakeLayoutNode(IEnumerator<Token> toks, bool embeddedLayout)
 		{
-			Node ret = new Node() { Type = NodeType.Layout, Children = new List<Node>() };
+			Node ret = new Node(NodeType.Layout, "");
 
 			var text = new StringBuilder();
 			Action dumpTextNodeIfNotEmpty = () =>
 			{
 				if (text.Length > 0)
 				{
-					ret.Children.Add(new Node() { Type = NodeType.Text, Data = text.ToString() });
+					ret.Children.Add(new Node(NodeType.Text, text.ToString()));
 					text.Clear();
 				}
 			};
@@ -148,15 +158,13 @@ namespace LogJoint
 
 		static Node MakeRendererNode(IEnumerator<Token> toks)
 		{
-			Node ret = new Node() { Type = NodeType.Renderer, Children = new List<Node>() };
-
-			ret.Data = ReadName(toks);
+			Node ret = new Node(NodeType.Renderer, ReadName(toks));
 
 			while (toks.Current.Type == TokenType.ParamColon)
 			{
-				Node? param = MakeParamNode(toks);
+				Node param = MakeParamNode(toks);
 				if (param != null)
-					ret.Children.Add(param.Value);
+					ret.Children.Add(param);
 			}
 
 			return ret;
@@ -174,13 +182,12 @@ namespace LogJoint
 				else
 					break;
 			}
-			return ret.ToString();
+			return ret.ToString().ToLower().Trim();
 		}
 
-		static Node? MakeParamNode(IEnumerator<Token> toks)
+		static Node MakeParamNode(IEnumerator<Token> toks)
 		{
-			Node ret = new Node() { Type = NodeType.RendererParam, Children = new List<Node>() };
-			ret.Data = ReadName(toks);
+			Node ret = new Node(NodeType.RendererParam, ReadName(toks));
 			if (ret.Data.Length == 0)
 				return null;
 			if (toks.Current.Type != TokenType.ParamEq)
@@ -193,60 +200,196 @@ namespace LogJoint
 		enum OutputFieldType
 		{
 			None = 0,
-			DateTime = 1,
-			Severity = 2,
-			Thread = 3
+			Date = 1,
+			Time = 2,
+			Severity = 3,
+			Thread = 4
 		};
 
-		//struct ProcessedToken
-		//{
-		//    public string Regex;
-		//    public OutputFieldType Field;
-		//};
+		struct NodeRegex
+		{
+			public string Regex;
+			public string Comment;
+			public OutputFieldType Field;
+			public NodeRegex(string re, string comment = "", OutputFieldType field = OutputFieldType.None)
+			{
+				Regex = re;
+				Comment = comment;
+				Field = field;
+			}
+		};
 
-		//struct ParsedRenderer
-		//{
-		//    public string Name;
-		//    public Dictionary<string, string> Params;
-		//};
+		enum StringCaseContext
+		{
+			None,
+			Upper,
+			Lower
+		};
 
-		//ParsedRenderer ParseRenderer(string rendererString)
-		//{
-		//    string escapedColonReplacement = "<!!colon!!>";
-		//    string escapedEqReplacement = "<!!eq!!>";
-		//    string[] splitRenderer = rendererString
-		//        .Replace(@"\:", escapedColonReplacement)
-		//        .Replace(@"\=", escapedEqReplacement)
-		//        .Split(':');
-		//    Regex escapeSequenceRe = new Regex(@"\\(.)?");
-		//    Func<string, string> unescapeAndNormalize = (s) => 
-		//        escapeSequenceRe.Replace(
-		//            s.Replace(escapedColonReplacement, @"\:").Replace(escapedEqReplacement, @"\="),
-		//            m => m.Groups[1].Value);
-		//    ParsedRenderer ret = new ParsedRenderer();
-		//    if (splitRenderer.Length == 0)
-		//        return ret;
-		//    ret.Name = unescapeAndNormalize(splitRenderer[0]);
-		//    ret.Params = new Dictionary<string, string>();
-		//    foreach (var splitParam in splitRenderer
-		//        .Skip(1) // skip renderer name
-		//        .Select(p => p.Split('=')) // split param to name->value pairs. pair is represented as array.
-		//        .Where(p => p.Length > 0) // skip empty pairs with no name
-		//    )
-		//        ret.Params[unescapeAndNormalize(splitParam[0])] = unescapeAndNormalize(splitParam.ElementAtOrDefault(1) ?? "");
-		//    return ret;
-		//}
+		struct NodeRegexContext
+		{
+			public StringCaseContext StringCase;
+			public NodeRegexContext SetStringCaseIf(StringCaseContext scase, Func<bool> predicate)
+			{
+				var ret = this;
+				if (predicate())
+					ret.StringCase = scase;
+				return ret;
+			}
+			public string ApplyCase(string s)
+			{
+				switch (StringCase)
+				{
+					case StringCaseContext.Lower:
+						return s.ToLower();
+					case StringCaseContext.Upper:
+						return s.ToUpper();
+					default:
+						return s;
+				}
+			}
+		};
 
-		//ProcessedToken ProcessToken(Token t)
-		//{
-		//    if (t.Type == TokenType.Literal)
-		//        return new ProcessedToken() { Regex = Regex.Escape(t.Value) };
-		//    var parsedRenderer = ParseRenderer(t.Value);
-		//    switch (parsedRenderer.Name)
-		//    {
-		//    }
+		static IEnumerable<NodeRegex> GetNodeRegexps(Node n, NodeRegexContext ctx)
+		{
+			switch (n.Type)
+			{
+				case NodeType.Text:
+					return EnumOne(new NodeRegex(Regex.Escape(ctx.ApplyCase(n.Data)), "fixed string '"+n.Data+"'" ));
+				case NodeType.Layout:
+					return n.Children.SelectMany(c => GetNodeRegexps(c, ctx));
+				case NodeType.Renderer:
+					return GetRendererNodeRegexps(n, ctx);
+				default:
+					return Enumerable.Empty<NodeRegex>();
+			}
+		}
 
-		//    return new ProcessedToken();
-		//}
+		static IEnumerable<NodeRegex> GetRendererNodeRegexps(Node renderer, NodeRegexContext ctx)
+		{
+			switch (renderer.Data)
+			{
+				case "shortdate":
+					return EnumOne(new NodeRegex(
+						@"\d{4}\-\d{2}\-\d{2}", 
+						"short date yyyy-MM-dd", 
+						OutputFieldType.Date));
+				case "literal":
+					return GetParamValue(renderer, "text").Select(
+						text => new NodeRegex(Regex.Escape(ctx.ApplyCase(text)), "literal '"+text+"'"));
+				case "uppercase":
+					return GetInnerLayout(renderer).SelectMany(
+						inner => GetNodeRegexps(
+							inner,
+							ctx.SetStringCaseIf(StringCaseContext.Upper, 
+								() => !IsPropertyFalse(renderer, "uppercase")
+							)
+						)
+					);
+				default:
+					return Enumerable.Empty<NodeRegex>();
+			}
+		}
+
+		static bool IsPropertyFalse(Node n, string name)
+		{
+			return GetNormalizedParamValue(n, name).Where(val => val == "false").Any();
+		}
+
+		static string GetInnerText(Node n)
+		{
+			switch (n.Type)
+			{
+				case NodeType.Text:
+					return n.Data;
+				case NodeType.Layout:
+				case NodeType.RendererParam:
+					return n.Children.Aggregate(new StringBuilder(), (sb, child) => sb.Append(GetInnerText(child))).ToString();
+				default:
+					return "";
+			}
+		}
+
+		static IEnumerable<Node> GetParam(Node renderer, string paramName)
+		{
+			return renderer.Children.Where(param => param.Type == NodeType.RendererParam && param.Data == paramName).Take(1);
+		}
+
+		static IEnumerable<string> GetNormalizedParamValue(Node renderer, string paramName)
+		{
+			return GetParamValue(renderer, paramName).Select(val => val.ToLower().Trim());
+		}
+
+		static IEnumerable<string> GetParamValue(Node renderer, string paramName)
+		{
+			return GetParam(renderer, paramName).Select(param => GetInnerText(param));
+		}
+
+		static IEnumerable<NodeRegex> EnumOne(NodeRegex value)
+		{
+			yield return value;
+		}
+
+		static IEnumerable<Node> GetInnerLayout(Node renderer)
+		{
+			return GetParam(renderer, "inner").SelectMany(
+				param => param.Children.Where(param2 => param2.Type == NodeType.Layout)).Take(1);
+		}
+
+		static Node ConvertAmbientPropertiesToNodes(Node root)
+		{
+			for (int i = 0; i < root.Children.Count; ++i)
+				root.Children[i] = ConvertAmbientPropertiesToNodes(root.Children[i]);
+			if (root.Type == NodeType.Renderer)
+				root = ConvertRendererAmbientPropertiesToNodes(root);
+			return root;
+		}
+
+		static void ConvertRendererAmbientPropertiesToNodesHelper(List<Node> foundAmbientRenderers, Node renderer, string testedRendererName, params string[] testedRendererParamNames)
+		{
+			if (renderer.Data != testedRendererName.ToLower())
+			{
+				var testedRendererParams = new List<Node>();
+				for (int i = 0; i < renderer.Children.Count;)
+				{
+					var param = renderer.Children[i];
+					if (param.Type == NodeType.RendererParam 
+					 && testedRendererParamNames.Count(s => string.Compare(param.Data, s, true) == 0) > 0)
+					{
+						testedRendererParams.Add(param);
+						renderer.Children.RemoveAt(i);
+					}
+					else
+					{
+						++i;
+					}
+				}
+				if (testedRendererParams.Count > 0)
+				{
+					foundAmbientRenderers.Add(new Node(NodeType.Renderer, testedRendererName, testedRendererParams.ToArray()));
+				}
+			}
+		}
+
+		static Node ConvertRendererAmbientPropertiesToNodes(Node root)
+		{
+			var tmp = new List<Node>();
+			ConvertRendererAmbientPropertiesToNodesHelper(tmp, root, "cached", "cached");
+			ConvertRendererAmbientPropertiesToNodesHelper(tmp, root, "filesystem-normalize", "fSNormalize");
+			ConvertRendererAmbientPropertiesToNodesHelper(tmp, root, "json-encode", "jsonEncode");
+			ConvertRendererAmbientPropertiesToNodesHelper(tmp, root, "lowercase", "lowercase");
+			ConvertRendererAmbientPropertiesToNodesHelper(tmp, root, "pad", "padCharacter", "padding", "fixedLength");
+			ConvertRendererAmbientPropertiesToNodesHelper(tmp, root, "trim-whitespace", "trimWhiteSpace");
+			ConvertRendererAmbientPropertiesToNodesHelper(tmp, root, "uppercase", "uppercase");
+			ConvertRendererAmbientPropertiesToNodesHelper(tmp, root, "when", "when");
+			ConvertRendererAmbientPropertiesToNodesHelper(tmp, root, "whenEmpty", "whenEmpty");
+			ConvertRendererAmbientPropertiesToNodesHelper(tmp, root, "xml-encode", "xmlEncode");
+			foreach (var ambRenderer in tmp)
+			{
+				ambRenderer.Children.Add(new Node(NodeType.RendererParam, "inner", new Node(NodeType.Layout, "", root)));
+				root = ambRenderer;
+			}
+			return root;
+		}
 	}
 }
--- trunk/model/tests/NLogLayoutImporterTest.cs
+++ trunk/model/tests/NLogLayoutImporterTest.cs
@@ -1,18 +1,44 @@
 ﻿using LogJoint;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Text;
+using System.Linq;
 using System.Xml;
+using NLog;
 
 namespace logjoint.model.tests
 {
 	[TestClass()]
 	public class NLogLayoutImporterTest
 	{
+		string CreateSimpleLog(string layout, Action<Logger> loggingCallback)
+		{
+			var target = new NLog.Targets.MemoryTarget();
+			target.Layout = layout;
+
+			NLog.LogManager.Configuration = new NLog.Config.LoggingConfiguration();
+			NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, NLog.LogLevel.Debug);
+
+			loggingCallback(NLog.LogManager.GetCurrentClassLogger());
+
+			return target.Logs.Aggregate(new StringBuilder(), (sb, line) => sb.AppendLine(line)).ToString();
+		}
+
+		void LogBasicLines(Logger l)
+		{
+			l.Debug("Hello");
+		}
+
 		[TestMethod()]
 		public void GenerateRegularGrammarElementTest()
 		{
+			var s1 = CreateSimpleLog(@"${literal:padCharacter=_:padding=4:fixedLength=True:text=aBcdefg}", LogBasicLines);
+			var s2 = CreateSimpleLog(@"${shortdate} ${pad:padCharacter= :padding=100:fixedLength=True:inner=${message}}", LogBasicLines);
+
 			NLogLayoutImporter.GenerateRegularGrammarElement(null,
-				@"aa\}bb\\cc\tdd ${literal:text=S\}t\\r\:ing} ${shortdate} ${pad:padCharacter= :padding=100:fixedLength=True:inner=${message}} xyz");
+				@"${shortdate}|${uppercase:uppercase=True:padding=10:inner=${literal:text=S\}t\\r\:ing}}");
+
+			// @"aa\}bb\\cc\tdd ${literal:text=S\}t\\r\:ing} ${shortdate} ${pad:padCharacter= :padding=100:fixedLength=True:inner=${message}} xyz"
 
 		}
 
@@ -21,5 +47,7 @@ namespace logjoint.model.tests
 		//    2. Embedded layouts
 		//    3. Significant spaces in layouts (like ${pad:padCharacter= })
 		//    4. Single \ at the end of layout string
+		//    5. Renderer or param name uppercase
+		//    6. Embedded renderers with ambient props
 	}
 }
