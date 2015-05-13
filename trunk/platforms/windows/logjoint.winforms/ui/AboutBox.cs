0a6fc9d0a07ff3e45ad92f5d402b7e281b52c5be
Sergey Sukhanov <sergeysu@microsoft.com>
2015-05-13 23:16:59 +0200

    About box: show download link to be shared with others

--- trunk/platforms/windows/logjoint.winforms/ui/AboutBox.cs
+++ trunk/platforms/windows/logjoint.winforms/ui/AboutBox.cs
@@ -15,15 +15,26 @@ namespace LogJoint.UI
 		{
 			InitializeComponent();
 
-
 			textBox.Text = string.Format(
 				"LogJoint{0}" +
 				"Log viewer tool for professionals.{0}" +
 				"Assembly version: {1}{0}" +
-				"http://logjoint.codeplex.com/",
+				"http://logjoint.codeplex.com/"+
+				"{2}",
 				Environment.NewLine,
-				Assembly.GetExecutingAssembly().GetName().Version
+				Assembly.GetExecutingAssembly().GetName().Version,
+				GetSharingText()
 			);
+		}
+
+		string GetSharingText()
+		{
+			var autoUpdateUrl = LogJoint.Properties.Settings.Default.AutoUpdateUrl;
+			if (string.IsNullOrEmpty(autoUpdateUrl))
+				return "";
+			return string.Format("{0}{0}Share the tool with other professionals: {1}",
+				Environment.NewLine,
+				autoUpdateUrl);
 		}
 	}
 }
