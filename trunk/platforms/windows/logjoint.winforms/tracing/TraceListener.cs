c29459f269fa6fb0df8e24b35f8d1f1d21e940f0
Sergey Sukhanov <sergeysu@microsoft.com>
2015-05-13 16:57:19 +0200

    Fixed trace listener not to fail secondary LJ instances

--- trunk/platforms/windows/logjoint.winforms/tracing/TraceListener.cs
+++ trunk/platforms/windows/logjoint.winforms/tracing/TraceListener.cs
@@ -1,13 +1,103 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace LogJoint
 {
-	public class TraceListener : System.Diagnostics.TextWriterTraceListener
+	public class TraceListener : System.Diagnostics.TraceListener
 	{
+		readonly Lazy<TextWriter> writer;
+
 		public TraceListener(string logFileName)
 			: base(logFileName)
 		{
-			base.Writer = new StreamWriter(logFileName, false);
+			writer = new Lazy<TextWriter>(() =>
+			{
+				try
+				{
+					return new StreamWriter(logFileName, false);
+				}
+				catch
+				{
+					return null;
+				}
+			}, true);
+		}
+
+		public override void Close()
+		{
+			if (!writer.IsValueCreated)
+				return;
+			try
+			{
+				writer.Value.Close();
+			}
+			catch (ObjectDisposedException)
+			{
+			}
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			try
+			{
+				if (disposing)
+					this.Close();
+			}
+			finally
+			{
+				base.Dispose(disposing);
+			}
+		}
+
+		public override void Flush()
+		{
+			var w = writer.Value;
+			if (w == null)
+				return;
+			try
+			{
+				w.Flush();
+			}
+			catch (ObjectDisposedException)
+			{
+			}
+		}
+
+		public override void Write(string message)
+		{
+			var w = writer.Value;
+			if (w == null)
+				return;
+			if (base.NeedIndent)
+			{
+				this.WriteIndent();
+			}
+			try
+			{
+				w.Write(message);
+			}
+			catch (ObjectDisposedException)
+			{
+			}
+		}
+
+		public override void WriteLine(string message)
+		{
+			var w = writer.Value;
+			if (w == null)
+				return;
+			if (base.NeedIndent)
+			{
+				this.WriteIndent();
+			}
+			try
+			{
+				w.WriteLine(message);
+				base.NeedIndent = true;
+			}
+			catch (ObjectDisposedException)
+			{
+			}
 		}
 	}
 }
