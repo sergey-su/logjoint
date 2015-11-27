ca9d8466d92b0f86cad2f6662e30226615bff52e
Sergey Sukhanov <sergeysu@microsoft.com>
2015-11-27 16:17:26 +0100

    .

--- trunk/tools/logjoint.updater/Program.cs
+++ trunk/tools/logjoint.updater/Program.cs
@@ -33,8 +33,10 @@ namespace logjoint.updater
 					try
 					{
 						Log("Waiting all logjoints to quit");
-						LogJointIPC.WaitUntilAllLogJointProcessesQuit(logJointProcessesTrackingIPCKey);						
+						LogJointIPC.WaitUntilAllLogJointProcessesQuit(logJointProcessesTrackingIPCKey);
 						Log("No logjoint left. Starting update.");
+
+						Thread.Sleep(1000); // give more time for just quited LJ binaries to be released by the OS
 
 						DoAndLog(() => Directory.Move(installationDir, oldInstallationDir), "rename " + installationDir + " to " + oldInstallationDir);
 						DoAndLog(() => Directory.Move(tempInstallationDir, installationDir), "rename " + tempInstallationDir + " to " + installationDir);
