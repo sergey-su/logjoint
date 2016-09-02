3ed4dfd1cded1cce17587db7a27a7017dd0bc881
Sergey Sukhanov <sergeysu@microsoft.com>
2016-09-02 21:40:13 +0200

    utility function that allows awaiting on a Process for the exit code

--- trunk/model/generic/TaskUtils.cs
+++ trunk/model/generic/TaskUtils.cs
@@ -6,6 +6,7 @@ using System.Xml;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Runtime.CompilerServices;
+using System.Diagnostics;
 
 namespace LogJoint
 {
@@ -162,6 +163,29 @@ namespace LogJoint
 			if (t.IsCanceled)
 				return new TaskCanceledException();
 			return null;
+		}
+
+		public static async Task<int> GetExitCodeAsync(this Process process, TimeSpan timeout)
+		{
+			var tcs = new TaskCompletionSource<int>();
+			EventHandler handler = (s, e) => tcs.TrySetResult(process.ExitCode);
+			process.EnableRaisingEvents = true;
+			process.Exited += handler;
+			try
+			{
+				if (process.HasExited)
+					return process.ExitCode;
+				await Task.WhenAny(Task.Delay(timeout), tcs.Task);
+				if (process.HasExited)
+					return process.ExitCode;
+				throw new TimeoutException(string.Format("Process {0} {1} did not exit in time",
+					process.Id,
+					process.StartInfo != null ? Path.GetFileName(process.StartInfo.FileName) : "<uknown image>"));
+			}
+			finally
+			{
+				process.Exited -= handler;
+			}
 		}
 	};
 
