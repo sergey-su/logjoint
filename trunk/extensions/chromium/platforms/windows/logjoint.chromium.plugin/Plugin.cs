066f4ad40a9c776905226eff73e07f2b88148522
Sergey Sukhanov <sergeysu@microsoft.com>
2017-06-26 18:01:29 +0200

    added chromium plugin skeleton + chrome_debug format definition

---
+++ trunk/extensions/chromium/platforms/windows/logjoint.chromium.plugin/Plugin.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using LogJoint.Extensibility;
+
+namespace LogJoint
+{
+	public class Plugin : PluginBase
+	{
+		public Plugin()
+		{
+		}
+
+		public override void Init(IApplication app)
+		{
+		}
+
+
+		public override void Dispose()
+		{
+		}
+	}
+}
