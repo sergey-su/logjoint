03dc9a31a9ab9ccdc96710cf3e805962e34a1772
sergeysu <sergeysu@microsoft.com>
2017-06-30 22:12:39 +0200

    mac: fixing build

--- trunk/extensions/chromium/platforms/osx/logjoint.chromium.plugin/Plugin.cs
+++ trunk/extensions/chromium/platforms/osx/logjoint.chromium.plugin/Plugin.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Globalization;
 using LogJoint.Extensibility;
 using LogJoint.Chromium;
 
@@ -10,22 +5,13 @@ namespace LogJoint
 {
 	public class Plugin : PluginBase
 	{
-		IPostprocessorsRegistry postprocessorsRegistry;
-
 		public Plugin()
 		{
 		}
 
 		public override void Init(IApplication app)
 		{
-			app.Model.Postprocessing.TimeSeriesTypes.DefaultTimeSeriesTypesAssembly = typeof(Chromium.TimeSeries.PostprocessorsFactory).Assembly;
-
-			postprocessorsRegistry = new PostprocessorsInitialilizer(
-				app.Model.Postprocessing.PostprocessorsManager, 
-				app.Model.UserDefinedFormatsManager, 
-				new Chromium.StateInspector.PostprocessorsFactory(),
-				new Chromium.TimeSeries.PostprocessorsFactory(app.Model.Postprocessing.TimeSeriesTypes)
-			);
+			PluginInitializer.Init(app);
 		}
 
 
