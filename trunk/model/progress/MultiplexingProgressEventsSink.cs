20095bc63971688d1c8cd6307b203a6d72fe7385
sergeysu <sergeysu@microsoft.com>
2017-07-10 21:37:40 +0300

    expand all positive filters from complex searches on search results view

---
+++ trunk/model/progress/MultiplexingProgressEventsSink.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LogJoint.Progress
+{
+	public class MultiplexingProgressEventsSink : IProgressEventsSink
+	{
+		readonly List<IProgressEventsSink> sinks;
+
+		public MultiplexingProgressEventsSink(IEnumerable<IProgressEventsSink> sinks)
+		{
+			this.sinks = sinks.ToList();
+		}
+
+		void IDisposable.Dispose()
+		{
+			sinks.ForEach(s => s.Dispose());
+		}
+
+		void IProgressEventsSink.SetValue(double value)
+		{
+			sinks.ForEach(s => s.SetValue(value));
+		}
+	};
+}
