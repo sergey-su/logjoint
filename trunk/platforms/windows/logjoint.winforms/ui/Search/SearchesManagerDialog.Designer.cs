cee1beb90e3e31b376231ffdf89d3d01e244547d
Sergey Sukhanov <sergeysu@microsoft.com>
2017-08-05 16:09:21 +0200

    win: implemented search editing UI

---
+++ trunk/platforms/windows/logjoint.winforms/ui/Search/SearchesManagerDialog.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace LogJoint.UI
+{
+	partial class SearchesManagerDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listView = new System.Windows.Forms.ListView();
+			this.addButton = new System.Windows.Forms.Button();
+			this.deleteButton = new System.Windows.Forms.Button();
+			this.editButton = new System.Windows.Forms.Button();
+			this.exportButton = new System.Windows.Forms.Button();
+			this.closeButton = new System.Windows.Forms.Button();
+			this.importButton = new System.Windows.Forms.Button();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.SuspendLayout();
+			// 
+			// listView
+			// 
+			this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+			this.listView.FullRowSelect = true;
+			this.listView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+			this.listView.HideSelection = false;
+			this.listView.Location = new System.Drawing.Point(12, 45);
+			this.listView.MinimumSize = new System.Drawing.Size(50, 50);
+			this.listView.Name = "listView";
+			this.listView.Size = new System.Drawing.Size(622, 281);
+			this.listView.TabIndex = 0;
+			this.listView.UseCompatibleStateImageBehavior = false;
+			this.listView.View = System.Windows.Forms.View.Details;
+			this.listView.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView_ItemSelectionChanged);
+			this.listView.Layout += new System.Windows.Forms.LayoutEventHandler(this.listView_Layout);
+			// 
+			// addButton
+			// 
+			this.addButton.Location = new System.Drawing.Point(12, 12);
+			this.addButton.Name = "addButton";
+			this.addButton.Size = new System.Drawing.Size(90, 27);
+			this.addButton.TabIndex = 1;
+			this.addButton.Text = "Add...";
+			this.addButton.UseVisualStyleBackColor = true;
+			this.addButton.Click += new System.EventHandler(this.addButton_Click);
+			// 
+			// deleteButton
+			// 
+			this.deleteButton.Location = new System.Drawing.Point(108, 12);
+			this.deleteButton.Name = "deleteButton";
+			this.deleteButton.Size = new System.Drawing.Size(90, 27);
+			this.deleteButton.TabIndex = 2;
+			this.deleteButton.Text = "Delete";
+			this.deleteButton.UseVisualStyleBackColor = true;
+			this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+			// 
+			// editButton
+			// 
+			this.editButton.Location = new System.Drawing.Point(204, 12);
+			this.editButton.Name = "editButton";
+			this.editButton.Size = new System.Drawing.Size(90, 27);
+			this.editButton.TabIndex = 2;
+			this.editButton.Text = "Edit...";
+			this.editButton.UseVisualStyleBackColor = true;
+			this.editButton.Click += new System.EventHandler(this.editButton_Click);
+			// 
+			// exportButton
+			// 
+			this.exportButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.exportButton.Location = new System.Drawing.Point(12, 339);
+			this.exportButton.Name = "exportButton";
+			this.exportButton.Size = new System.Drawing.Size(90, 27);
+			this.exportButton.TabIndex = 1;
+			this.exportButton.Text = "Export...";
+			this.exportButton.UseVisualStyleBackColor = true;
+			this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+			// 
+			// closeButton
+			// 
+			this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.closeButton.Location = new System.Drawing.Point(544, 339);
+			this.closeButton.Name = "closeButton";
+			this.closeButton.Size = new System.Drawing.Size(90, 27);
+			this.closeButton.TabIndex = 1;
+			this.closeButton.Text = "Close";
+			this.closeButton.UseVisualStyleBackColor = true;
+			this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+			// 
+			// importButton
+			// 
+			this.importButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.importButton.Location = new System.Drawing.Point(122, 339);
+			this.importButton.Name = "importButton";
+			this.importButton.Size = new System.Drawing.Size(90, 27);
+			this.importButton.TabIndex = 1;
+			this.importButton.Text = "Import...";
+			this.importButton.UseVisualStyleBackColor = true;
+			this.importButton.Click += new System.EventHandler(this.importButton_Click);
+			// 
+			// SearchesManagerDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.CancelButton = this.closeButton;
+			this.ClientSize = new System.Drawing.Size(646, 381);
+			this.Controls.Add(this.editButton);
+			this.Controls.Add(this.deleteButton);
+			this.Controls.Add(this.closeButton);
+			this.Controls.Add(this.importButton);
+			this.Controls.Add(this.exportButton);
+			this.Controls.Add(this.addButton);
+			this.Controls.Add(this.listView);
+			this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.MinimizeBox = false;
+			this.Name = "SearchesManagerDialog";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Filters manager";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView listView;
+		private System.Windows.Forms.Button addButton;
+		private System.Windows.Forms.Button deleteButton;
+		private System.Windows.Forms.Button editButton;
+		private System.Windows.Forms.Button exportButton;
+		private System.Windows.Forms.Button closeButton;
+		private System.Windows.Forms.Button importButton;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+	}
+}
---
+++ trunk/platforms/windows/logjoint.winforms/ui/Search/SearchesManagerDialog.cs
@@ -0,0 +1,119 @@
+﻿using LogJoint.UI.Presenters.SearchesManagerDialog;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LogJoint.UI
+{
+	public partial class SearchesManagerDialog : Form, IDialogView
+	{
+		readonly IDialogViewEvents eventsHandler;
+		readonly Dictionary<ViewControl, Control> controls;
+
+		public SearchesManagerDialog(IDialogViewEvents eventsHandler)
+		{
+			InitializeComponent();
+			this.eventsHandler = eventsHandler;
+			this.controls = new Dictionary<ViewControl, Control>()
+			{
+				{ ViewControl.AddButton, addButton },
+				{ ViewControl.DeleteButton, deleteButton },
+				{ ViewControl.EditButton, editButton },
+				{ ViewControl.Export, exportButton },
+				{ ViewControl.Import, importButton },
+			};
+		}
+
+		ViewItem[] IDialogView.SelectedItems
+		{
+			get
+			{
+				return listView.SelectedItems.OfType<ListViewItem>().Select(i => i.Tag).OfType<ViewItem>().ToArray();
+			}
+			set
+			{
+				var lookup = value.ToLookup(i => i);
+				listView.SelectedIndices.Clear();
+				foreach (ListViewItem i in listView.Items)
+					if (lookup.Contains(i.Tag as ViewItem))
+						listView.SelectedIndices.Add(i.Index);
+			}
+		}
+
+		void IDialogView.CloseModal()
+		{
+			DialogResult = DialogResult.OK;
+		}
+
+		void IDialogView.EnableControl(ViewControl id, bool value)
+		{
+			Control ctrl;
+			if (controls.TryGetValue(id, out ctrl))
+				ctrl.Enabled = value;
+		}
+
+		void IDialogView.OpenModal()
+		{
+			ShowDialog();
+		}
+
+		void IDialogView.SetItems(ViewItem[] items)
+		{
+			listView.Items.Clear();
+			listView.Items.AddRange(items.Select(i => new ListViewItem(new[] { i.Caption })
+			{
+				Tag = i
+			}).ToArray());
+		}
+
+		private void addButton_Click(object sender, EventArgs e)
+		{
+			eventsHandler.OnAddClicked();
+		}
+
+		private void deleteButton_Click(object sender, EventArgs e)
+		{
+			eventsHandler.OnDeleteClicked();
+		}
+
+		private void editButton_Click(object sender, EventArgs e)
+		{
+			eventsHandler.OnEditClicked();
+		}
+
+		private void exportButton_Click(object sender, EventArgs e)
+		{
+			eventsHandler.OnExportClicked();
+		}
+
+		private void importButton_Click(object sender, EventArgs e)
+		{
+			eventsHandler.OnImportClicked();
+		}
+
+		private void closeButton_Click(object sender, EventArgs e)
+		{
+			eventsHandler.OnCloseClicked();
+		}
+
+		private void listView_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+		{
+			eventsHandler.OnSelectionChanged();
+		}
+
+		private void listView_Layout(object sender, LayoutEventArgs e)
+		{
+			columnHeader1.Width = listView.ClientSize.Width - 2;
+		}
+	}
+
+	class SearchesManagerDialogView : IView
+	{
+		IDialogView IView.CreateDialog(IDialogViewEvents eventsHandler)
+		{
+			return new SearchesManagerDialog(eventsHandler);
+		}
+	};
+}
