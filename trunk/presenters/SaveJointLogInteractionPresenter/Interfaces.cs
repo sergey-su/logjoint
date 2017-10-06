440a253676da602e93cac51b32d0d59c4352387d
sergeysu <suhanovsergey@gmail.com>
2017-10-06 21:57:06 +0200

    implemented saving joint log properly; win only so far

---
+++ trunk/presenters/SaveJointLogInteractionPresenter/Interfaces.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace LogJoint.UI.Presenters.SaveJointLogInteractionPresenter
+{
+	public interface IPresenter
+	{
+		void StartInteraction();
+		bool IsInteractionInProgress { get; }
+	};
+};
