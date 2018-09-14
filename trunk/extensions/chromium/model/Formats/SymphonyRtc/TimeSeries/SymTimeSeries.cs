5811b7b2c6479171e5f04174f8fee479e66ab20c
sergeysu <sergey.sukhanov@symphony.com>
2018-09-14 18:02:22 +0200

    added WebRTC timeseries support for symphony logs

---
+++ trunk/extensions/chromium/model/Formats/SymphonyRtc/TimeSeries/SymTimeSeries.cs
@@ -0,0 +1,84 @@
+﻿using LogJoint.Analytics.TimeSeries;
+
+namespace LogJoint.Symphony.Rtc
+{
+	static class Consts
+	{
+		internal const string objIdRe = @"^(?<id>[\w\-]+\s\|\s[^\.]+)\.";
+		internal const string type = "WebRTC";
+		internal const string prefix = "RTC";
+	};
+
+	[TimeSeriesEvent(Type = Consts.type)]
+	[Expression(Consts.objIdRe + @"(?<name>bytesSent|bytesReceived)=<value:int>", Prefix = Consts.prefix)]
+	[Source(From = "id")]
+	public class Kilobytes
+	{
+		[TimeSeries(From = "value", Name = "name", Unit = "kB", Scale = 0.001)]
+		public double ts;
+	}
+
+	[TimeSeriesEvent(Type = Consts.type)]
+	[Expression(Consts.objIdRe + @"(?<name>framesSent|framesReceived|framesEncoded|framesDecoded|framesDropped|framesSent|hugeFramesSent)=<value:int>", Prefix = Consts.prefix)]
+	[Source(From = "id")]
+	public class Frames
+	{
+		[TimeSeries(From = "value", Name = "name", Unit = "Frames")]
+		public double ts;
+	}
+
+	[TimeSeriesEvent(Type = Consts.type)]
+	[Expression(Consts.objIdRe + @"(?<name>messagesReceived|messagesSent|consentRequestsSent|requestsReceived|requestsSent|responsesSent)=<value:int>", Prefix = Consts.prefix)]
+	[Source(From = "id")]
+	public class Messages
+	{
+		[TimeSeries(From = "value", Name = "name", Unit = "Messages")]
+		public double ts;
+	}
+
+	[TimeSeriesEvent(Type = Consts.type)]
+	[Expression(Consts.objIdRe + @"(?<name>packetsSent|packetsLost|packetsReceived|firCount|nackCount|pliCount)=<value:int>", Prefix = Consts.prefix)]
+	[Source(From = "id")]
+	public class Packets
+	{
+		[TimeSeries(From = "value", Name = "name", Unit = "Packets")]
+		public double ts;
+	}
+
+	[TimeSeriesEvent(Type = Consts.type)]
+	[Expression(Consts.objIdRe + @"(?<name>availableIncomingBitrate|availableOutgoingBitrate)=<value:double>", Prefix = Consts.prefix)]
+	[Source(From = "id")]
+	public class Bitrate
+	{
+		[TimeSeries(From = "value", Name = "name", Unit = "Kbps", Scale = 0.001)]
+		public double ts;
+	}
+
+	[TimeSeriesEvent(Type = Consts.type)]
+	[Expression(Consts.objIdRe + @"(?<name>currentRoundTripTime|totalRoundTripTime|jitter|jitterBufferDelay|totalSamplesDuration)=<value:double>", Prefix = Consts.prefix)]
+	[Source(From = "id")]
+	public class Seconds
+	{
+		[TimeSeries(From = "value", Name = "name", Unit = "ms", Scale = 1000)]
+		public double ts;
+	}
+
+	[TimeSeriesEvent(Type = Consts.type)]
+	[Expression(Consts.objIdRe + @"(?<name>frameHeight|frameWidth)=<value:int>", Prefix = Consts.prefix)]
+	[Source(From = "id")]
+	public class Pixels
+	{
+		[TimeSeries(From = "value", Name = "name", Unit = "Pixels")]
+		public double ts;
+	}
+
+
+	[TimeSeriesEvent(Type = Consts.type)]
+	[Expression(Consts.objIdRe + @"(?<name>fractionLost|qpSum|concealedSamples|concealmentEvents|totalAudioEnergy|totalSamplesReceived|audioLevel|dataChannelsClosed|dataChannelsOpened)=<value:double>", Prefix = Consts.prefix)]
+	[Source(From = "id")]
+	public class Unitless
+	{
+		[TimeSeries(From = "value", Name = "name", Unit = "")]
+		public double ts;
+	}
+}
--- trunk/extensions/chromium/model/Postprocessors/TimeSeriesPostprocessorsFactory.cs
+++ trunk/extensions/chromium/model/Postprocessors/TimeSeriesPostprocessorsFactory.cs
@@ -8,6 +8,7 @@ using LogJoint.Analytics.TimeSeries;
 using LogJoint.Postprocessing.TimeSeries;
 using CDL = LogJoint.Chromium.ChromeDebugLog;
 using DMP = LogJoint.Chromium.WebrtcInternalsDump;
+using Sym = LogJoint.Symphony.Rtc;
 
 namespace LogJoint.Chromium.TimeSeries
 {
@@ -15,6 +16,7 @@ namespace LogJoint.Chromium.TimeSeries
 	{
 		ILogSourcePostprocessor CreateChromeDebugPostprocessor();
 		ILogSourcePostprocessor CreateWebRtcInternalsDumpPostprocessor();
+		ILogSourcePostprocessor CreateSymphonyRtcPostprocessor();
 	};
 
 	public class PostprocessorsFactory : IPostprocessorsFactory
@@ -43,6 +45,15 @@ namespace LogJoint.Chromium.TimeSeries
 				typeId, caption,
 				(doc, logSource) => DeserializeOutput(doc, logSource),
 				i => RunForWebRtcInternalsDump(new DMP.Reader(i.CancellationToken).Read(i.LogFileName, i.GetLogFileNameHint(), i.ProgressHandler), i.OutputFileName, i.CancellationToken, i.TemplatesTracker, i.InputContentsEtagAttr)
+			);
+		}
+
+		ILogSourcePostprocessor IPostprocessorsFactory.CreateSymphonyRtcPostprocessor()
+		{
+			return new LogSourcePostprocessorImpl(
+				typeId, caption,
+				(doc, logSource) => DeserializeOutput(doc, logSource),
+				i => RunForSymphonyRtc(new Sym.Reader(i.CancellationToken).Read(i.LogFileName, i.GetLogFileNameHint(), i.ProgressHandler), i.OutputFileName, i.CancellationToken, i.TemplatesTracker, i.InputContentsEtagAttr)
 			);
 		}
 
@@ -91,6 +102,27 @@ namespace LogJoint.Chromium.TimeSeries
 			ICombinedParser parser = new TimeSeriesCombinedParser(timeSeriesTypesAccess.GetMetadataTypes());
 
 			await parser.FeedLogMessages(input, m => m.ObjectId, m => m.Text);
+
+			TimeSeriesPostprocessorOutput.SerializePostprocessorOutput(
+				parser.GetParsedTimeSeries(),
+				parser.GetParsedEvents(),
+				outputFileName,
+				timeSeriesTypesAccess);
+		}
+
+		async Task RunForSymphonyRtc(
+			IEnumerableAsync<Sym.Message[]> input,
+			string outputFileName,
+			CancellationToken cancellation,
+			ICodepathTracker templatesTracker,
+			XAttribute contentsEtagAttr
+		)
+		{
+			timeSeriesTypesAccess.CheckForCustomConfigUpdate();
+
+			ICombinedParser parser = new TimeSeriesCombinedParser(timeSeriesTypesAccess.GetMetadataTypes());
+
+			await parser.FeedLogMessages(input, m => m.Text, m => string.Format("{0} | {1}", m.Logger, m.Text));
 
 			TimeSeriesPostprocessorOutput.SerializePostprocessorOutput(
 				parser.GetParsedTimeSeries(),
