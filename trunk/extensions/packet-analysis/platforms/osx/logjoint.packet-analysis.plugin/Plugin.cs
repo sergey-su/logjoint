3b57009746c6040e64868c1ba7ece0fe066168d9
sergeysu <sergey.sukhanov@symphony.com>
2019-07-19 23:14:28 +0200

    .

--- trunk/extensions/packet-analysis/platforms/osx/logjoint.packet-analysis.plugin/Plugin.cs
+++ trunk/extensions/packet-analysis/platforms/osx/logjoint.packet-analysis.plugin/Plugin.cs
@@ -1,6 +1,11 @@
 using M = LogJoint.PacketAnalysis;
 using V = LogJoint.PacketAnalysis.UI;
 using P = LogJoint.PacketAnalysis.UI.Presenters;
+using Foundation;
+using System.Reflection;
+using System.IO;
+using System;
+using System.Threading;
 
 namespace LogJoint
 {
@@ -8,21 +13,31 @@ namespace LogJoint
 	{
 		public Plugin(IApplication app)
 		{
-			M.PluginInitializer.Init(app, new ViewsFactory { app = app });
+			M.PluginInitializer.Init(app, new ViewsFactory(app));
 		}
 
 		class ViewsFactory : P.Factory.IViewsFactory
 		{
-			public IApplication app;
+			private readonly IApplication app;
+			private readonly Lazy<NSBundle> bundle;
+
+			public ViewsFactory(IApplication app)
+			{
+				this.app = app;
+				this.bundle = new Lazy<NSBundle>(
+					() => NSBundle.FromPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)),
+					LazyThreadSafetyMode.None
+				);
+			}
 
 			P.MessagePropertiesDialog.IView P.Factory.IViewsFactory.CreateMessageContentView()
 			{
-				return new V.MessageContentViewController(app.View);
+				return new V.MessageContentViewController(app.View, bundle.Value);
 			}
 
 			P.NewLogSourceDialog.Pages.WiresharkPage.IView P.Factory.IViewsFactory.CreateWiresharkPageView()
 			{
-				return new V.WiresharkPageAdapter();
+				return new V.WiresharkPageAdapter(bundle.Value);
 			}
 		};
 	}
