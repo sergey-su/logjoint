70c0a3b14f127b5b9d6ab0d4f6937277386c15ae
sergeysu <suhanovsergey@gmail.com>
2019-10-05 23:28:55 +0200

    migration to netstandard, phase 1

--- trunk/model/postprocessing/correlator/SolverCorrelator/Solvers/SolverFactory.cs
+++ trunk/model/postprocessing/correlator/SolverCorrelator/Solvers/SolverFactory.cs
@@ -8,20 +8,7 @@ namespace LogJoint.Postprocessing.Correlation
 	{
 		public static ISolver Create()
 		{
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-#if WIN
-				return new Postprocessing.Correlation.EmbeddedSolver.EmbeddedSolver();
-#else
-				throw new Exception("Unsupported platform");
-#endif
-			else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-#if MONOMAC
-				return new Postprocessing.Correlation.ExternalSolver.CmdLineToolProxy();
-#else
-				throw new Exception("Unsupported platform");
-#endif
-			else
-				throw new Exception("Unsupported platform");
+			return new Postprocessing.Correlation.EmbeddedSolver.EmbeddedSolver();
 		}
 	}
 }
