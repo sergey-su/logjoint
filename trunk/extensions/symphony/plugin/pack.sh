c3b6c91db3684adbd9bd60dfaec9ce1bd06b82eb
sergeysu <sergey.sukhanov@symphony.com>
2019-10-11 16:00:41 +0200

    symphony plugin: moved to use of SDK packages instead of direct project references

--- trunk/extensions/symphony/plugin/pack.sh
+++ trunk/extensions/symphony/plugin/pack.sh
@@ -1,1 +1,1 @@
-dotnet run --project ../../../sdk/tools/logjoint.plugintool pack bin/Debug/netstandard2.0/manifest.xml bin/symphony.zip $1
+dotnet logjoint.plugintool pack bin/Debug/netstandard2.0/manifest.xml bin/symphony.zip $1
