1972f277fd616d0dfd18fe13f86e030ba432472f
sergeysu <suhanovsergey@gmail.com>
2019-10-13 14:35:42 +0200

    presentation sdk: moved InternalsVisibleTo to correct place

--- trunk/sdk/presentation/Postprocessing/Interfaces.cs
+++ trunk/sdk/presentation/Postprocessing/Interfaces.cs
@@ -1,13 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// todo
-[assembly: InternalsVisibleTo("logjoint.presenters.tests")]
-[assembly: InternalsVisibleTo("logjoint.presenters")]
-[assembly: InternalsVisibleTo("logjoint.sdk")]
-
-namespace LogJoint.UI.Presenters.Postprocessing
+﻿namespace LogJoint.UI.Presenters.Postprocessing
 {
 	public interface IPostprocessorOutputForm
 	{
