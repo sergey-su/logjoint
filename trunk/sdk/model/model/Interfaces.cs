19fb74e43d72c63c3ff0abd081ad856413eb132f
sergeysu <suhanovsergey@gmail.com>
2019-10-13 14:31:23 +0200

    model sdk: moved InternalsVisibleTo to correct place

--- trunk/sdk/model/model/Interfaces.cs
+++ trunk/sdk/model/model/Interfaces.cs
@@ -2,16 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// todo
-[assembly: InternalsVisibleTo("logjoint.model.tests")]
-[assembly: InternalsVisibleTo("logjoint.presenters.tests")]
-[assembly: InternalsVisibleTo("logjoint.model")]
-[assembly: InternalsVisibleTo("logjoint.analytics")]
-[assembly: InternalsVisibleTo("logjoint")]
-[assembly: InternalsVisibleTo("logjoint.presenters")]
-[assembly: InternalsVisibleTo("logjoint.presentation.sdk")]
-[assembly: InternalsVisibleTo("logjoint.integration.tests")]
-
 namespace LogJoint
 {
 	/// <summary>
