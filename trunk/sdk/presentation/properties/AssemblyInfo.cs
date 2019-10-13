58d71510d8f136606bdb4d62e1e0e553753efdb8
sergeysu <suhanovsergey@gmail.com>
2019-10-13 14:35:42 +0200

    presentation sdk: moved InternalsVisibleTo to correct place

---
+++ trunk/sdk/presentation/properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: InternalsVisibleTo("logjoint.presenters.tests")]
+[assembly: InternalsVisibleTo("logjoint.presenters")]
+[assembly: InternalsVisibleTo("logjoint.sdk")]
