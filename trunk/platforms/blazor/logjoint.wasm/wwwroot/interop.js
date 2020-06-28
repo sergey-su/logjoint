49e1fc6d0c63daa04f3944a2f495a80f83f6b07a
sergeysu <suhanovsergey@gmail.com>
2020-06-28 22:20:45 +0200

    sdk -> preview 5

--- trunk/platforms/blazor/logjoint.wasm/wwwroot/interop.js
+++ trunk/platforms/blazor/logjoint.wasm/wwwroot/interop.js
@@ -2,6 +2,9 @@
     getElementWidth: function (e) {
         return e.getBoundingClientRect().width;
     },
+    getElementHeight: function (e) {
+        return e.getBoundingClientRect().height;
+    },
     getElementLeft: function (e) {
         return e.getBoundingClientRect().left;
     }
