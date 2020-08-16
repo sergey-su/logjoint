4078b0bca857795a502748461079b5097a44a12e
sergeysu <suhanovsergey@gmail.com>
2020-08-16 20:37:22 +0200

    blazor: sketched main window tabs view; implemented logs list via generic tree

---
+++ trunk/platforms/blazor/logjoint.wasm/Views/LogSourcesList/ViewProxy.cs
@@ -0,0 +1,27 @@
+﻿using LogJoint.UI.Presenters.SourcesList;
+using System;
+
+namespace LogJoint.Wasm.UI
+{
+	public class SourcesListViewProxy : IView
+	{
+		IViewModel viewModel;
+		IView component;
+
+		public void SetComponent(IView component)
+		{
+			this.component = component;
+			component?.SetViewModel(viewModel);
+		}
+
+		void IView.SetTopItem(IViewItem item)
+		{
+			component.SetTopItem(item);
+		}
+
+		void IView.SetViewModel(IViewModel value)
+		{
+			viewModel = value;
+		}
+	}
+}
