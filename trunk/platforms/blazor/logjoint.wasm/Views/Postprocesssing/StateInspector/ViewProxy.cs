5f1f3cb61919aea54abad0d73ce2f30a95736f1e
sergeysu <suhanovsergey@gmail.com>
2020-09-06 21:24:29 +0200

    blazor: state inspector wip

---
+++ trunk/platforms/blazor/logjoint.wasm/Views/Postprocesssing/StateInspector/ViewProxy.cs
@@ -0,0 +1,32 @@
+﻿using LogJoint.UI.Presenters.Postprocessing.StateInspectorVisualizer;
+
+namespace LogJoint.Wasm.UI.Postprocesssing.StateInspector
+{
+	public class ViewProxy : IView
+	{
+		IViewModel viewModel;
+		IView component;
+
+		public void SetComponent(IView component)
+		{
+			this.component = component;
+			component?.SetViewModel(viewModel);
+		}
+
+		void IView.ScrollStateHistoryItemIntoView(int itemIndex)
+		{
+			component?.ScrollStateHistoryItemIntoView(itemIndex);
+		}
+
+		void IView.SetViewModel(IViewModel value)
+		{
+			viewModel = value;
+			component?.SetViewModel(value);
+		}
+
+		void IView.Show()
+		{
+			component.Show();
+		}
+	}
+}
