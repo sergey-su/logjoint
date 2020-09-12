cba5d09e8e96d9148225a2c398fc405fb99c1319
sergeysu <suhanovsergey@gmail.com>
2020-09-12 21:41:45 +0200

    blazor: tree full-row selection

---
+++ trunk/platforms/blazor/logjoint.wasm/Views/Tree/TreeStyles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.JSInterop;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogJoint.Wasm.UI
+{
+	public class TreeStyles
+	{
+		private IJSRuntime jsRuntime;
+		private int maxLevelStyleGenerated = -1;
+
+		public TreeStyles(IJSRuntime jsRuntime)
+		{
+			this.jsRuntime = jsRuntime;
+		}
+
+		public async ValueTask EnsureNodeStyleExists(int level)
+		{
+			if (level > maxLevelStyleGenerated)
+			{
+				var styleBuilder = new StringBuilder();
+				var stylesToGenerate = 10;
+				for (int i = 0; i < stylesToGenerate; ++i)
+				{
+					int l = maxLevelStyleGenerated + 1 + i;
+					styleBuilder.AppendLine($".node.c{l} {{ padding-left: {20*l}px; }}");
+				}
+				maxLevelStyleGenerated += stylesToGenerate;
+				await jsRuntime.InvokeVoidAsync("logjoint.adoptStyle", styleBuilder.ToString());
+			}
+		}
+	}
+}
