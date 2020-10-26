535323aa73398c38f1df665c9df139357887d554
sergeysu <suhanovsergey@gmail.com>
2020-10-26 18:36:34 +0100

    blazor: wrap resize observer interop into a type-safe C# wrapper

--- trunk/platforms/blazor/logjoint.wasm/JsInterop/Resize.cs
+++ trunk/platforms/blazor/logjoint.wasm/JsInterop/Resize.cs
@@ -20,7 +20,7 @@ namespace LogJoint.Wasm
             Vertical
         };
 
-        public async ValueTask<IDisposable> AddResizeHandler(
+        public async ValueTask<IDisposable> InitResizer(
             ElementReference resizerElement,
             ElementReference targetElement,
             Direction direction,
@@ -29,19 +29,52 @@ namespace LogJoint.Wasm
             Action<double> handler
         )
         {
-            var resizeInvokeHelper = DotNetObjectReference.Create(new ResizeInvokeHelper { action = handler });
+            var resizeInvokeHelper = DotNetObjectReference.Create(new ResizerHelper { action = handler });
             await jsRuntime.InvokeVoidAsync(
                 direction == Direction.Horizonal ? "logjoint.resize.initEWResizer" : "logjoint.resize.initNSResizer",
                 resizerElement, targetElement, inverse, relativeToParent, handler != null ? resizeInvokeHelper : null);
             return resizeInvokeHelper;
         }
 
-        class ResizeInvokeHelper
+        public async ValueTask<IAsyncDisposable> ObserveResize(ElementReference element, Action handler)
+        {
+            var helperRef = DotNetObjectReference.Create(new ResizeObserverHelper { action = handler });
+            var jsHandle = await jsRuntime.InvokeAsync<long>("logjoint.resize.observe", element, helperRef);
+            return new ResizeObserverHandle
+            {
+                jsRuntime = jsRuntime,
+                jsHandle = jsHandle,
+                helperRef = helperRef,
+            };
+        }
+
+        class ResizerHelper
         {
             public Action<double> action;
 
             [JSInvokable]
             public void Invoke(double value) => action.Invoke(value);
         }
+
+        class ResizeObserverHelper
+        {
+            public Action action;
+
+            [JSInvokable]
+            public void OnResize() => action.Invoke();
+        }
+
+        class ResizeObserverHandle : IAsyncDisposable
+        {
+            public IJSRuntime jsRuntime;
+            public IDisposable helperRef;
+            public long jsHandle;
+
+            async ValueTask IAsyncDisposable.DisposeAsync()
+            {
+                helperRef.Dispose();
+                await jsRuntime.InvokeVoidAsync("logjoint.resize.unobserve", jsHandle);
+            }
+        };
     }
 }
