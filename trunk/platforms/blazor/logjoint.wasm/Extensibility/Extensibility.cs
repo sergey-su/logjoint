781f39be0908c4051193525836ce8f1f7c70ef7e
Sergey <sergeysu@google.com>
2021-11-03 18:27:03 +0100

    extract extensibility classes from main file

---
+++ trunk/platforms/blazor/logjoint.wasm/Extensibility/Extensibility.cs
@@ -0,0 +1,26 @@
+﻿
+namespace LogJoint.Wasm.Extensibility
+{
+    public interface IApplication
+    {
+        IModel Model { get; }
+        LogJoint.UI.Presenters.IPresentation Presentation { get; }
+        object View { get; }
+    };
+
+    class Application : IApplication
+    {
+        public Application(
+            IModel model,
+            LogJoint.UI.Presenters.IPresentation presentation
+        )
+        {
+            this.Model = model;
+            this.Presentation = presentation;
+        }
+
+        public LogJoint.UI.Presenters.IPresentation Presentation { get; private set; }
+        public IModel Model { get; private set; }
+        public object View { get; private set; }
+    }
+}
--- trunk/platforms/blazor/logjoint.wasm/Program.cs
+++ trunk/platforms/blazor/logjoint.wasm/Program.cs
@@ -14,32 +14,6 @@ using System.IO.Compression;
 
 namespace LogJoint.Wasm
 {
-    namespace Extensibility
-    {
-        public interface IApplication
-        {
-            IModel Model { get; }
-            LogJoint.UI.Presenters.IPresentation Presentation { get; }
-            object View { get; }
-        };
-
-        class Application : IApplication
-        {
-            public Application(
-                IModel model,
-                LogJoint.UI.Presenters.IPresentation presentation
-            )
-            {
-                this.Model = model;
-                this.Presentation = presentation;
-            }
-
-            public LogJoint.UI.Presenters.IPresentation Presentation { get; private set; }
-            public IModel Model { get; private set; }
-            public object View { get; private set; }
-        }
-    }
-
     public class Program
     {
         public static async Task Main(string[] args)
