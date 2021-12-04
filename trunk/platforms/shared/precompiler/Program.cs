9d7e09172eb56e94ea6e34414102a25b78132bc5
Sergey <sergeysu@google.com>
2021-12-04 22:55:47 +0100

    precompiler: support handling individual format files

--- trunk/platforms/shared/precompiler/Program.cs
+++ trunk/platforms/shared/precompiler/Program.cs
@@ -11,20 +11,32 @@ namespace LogJoint
 		static async Task Main(string[] args)
 		{
 			string pluginPath = null;
+			string formatFilePath = null;
 			if (args.ElementAtOrDefault(0) == "plugin")
 			{
 				pluginPath = args.ElementAtOrDefault(1);
 			}
+			if (args.ElementAtOrDefault(0) == "format")
+			{
+				formatFilePath = args.ElementAtOrDefault(1);
+			}
 
-			if (pluginPath == null)
+			if (pluginPath == null && formatFilePath == null)
 			{
 				Console.WriteLine("Usage:");
 				Console.WriteLine("   logjoint.precompiler plugin <plugin dirertory>  - precompile code for all format definitions in the plugin");
+				Console.WriteLine("   logjoint.precompiler format <format file>       - precompile code the format definition in the given file");
 				return;
 			}
 
 			var appDataDir = Path.Combine(Path.GetTempPath(),
 				$"logjoint.precompiler.workdir.{DateTime.Now:yyyy'-'MM'-'dd'T'HH'-'mm'-'ss'.'fff}");
+
+			var tempFormatsDir = Path.Combine(appDataDir, "TempFormats");
+			Directory.CreateDirectory(tempFormatsDir);
+			var tempFormatFilePath = Path.Combine(tempFormatsDir, "temp.format.xml");
+			if (formatFilePath != null)
+				File.Copy(formatFilePath, tempFormatFilePath);
 
 			ISynchronizationContext serialSynchronizationContext = new SerialSynchronizationContext();
 
@@ -42,7 +54,7 @@ namespace LogJoint
 						AppDataDirectory = appDataDir,
 						TraceListeners = new TraceListener[] { },
 						DisableLogjointInstancesCounting = true,
-						AdditionalFormatDirectories = new string[0] { },
+						AdditionalFormatDirectories = new string[] { tempFormatsDir },
 						UserCodeAssemblyProvider = new ComplingUserCodeAssemblyProvider(new DefaultMetadataReferencesProvider()),
 					},
 					serialSynchronizationContext,
@@ -55,56 +67,75 @@ namespace LogJoint
 
 			await serialSynchronizationContext.Invoke(() =>
 			{
-				modelObjects.PluginsManager.LoadPlugins(new
+				if (pluginPath != null)
 				{
-					Model = modelObjects.ExpensibilityEntryPoint,
-					Presentation = (LogJoint.UI.Presenters.IPresentation)null
-				}, pluginPath, preferTestPluginEntryPoints: true);
-
-				Extensibility.IPluginManifest pluginManifest = modelObjects.PluginsManager.InstalledPlugins.FirstOrDefault();
-				if (pluginManifest == null)
-				{
-					Console.WriteLine("ERROR: Failed to load plugin from '{0}'", pluginPath);
-					return;
+					HandlePlugin(pluginPath, modelObjects);
 				}
-				foreach (Extensibility.IPluginFile formatFile in pluginManifest.Files.Where(f => f.Type == Extensibility.PluginFileType.FormatDefinition))
+
+				if (formatFilePath != null)
 				{
-					IUserDefinedFactory formatFactory = modelObjects.UserDefinedFormatsManager.Items.FirstOrDefault(
-						factory => factory.Location == formatFile.AbsolutePath);
-					if (formatFactory == null)
-					{
-						Console.WriteLine("ERROR: Failed to load format definition '{0}'", formatFile.AbsolutePath);
-						continue;
-					}
-					var precomp = formatFactory as IPrecompilingLogProviderFactory;
-					if (precomp == null)
-					{
-						Console.WriteLine("WARNING: Skipping '{0}' - precompilation is not supported for it", formatFile.AbsolutePath);
-						continue;
-					}
-					var manifestDoc = XDocument.Load(formatFile.AbsolutePath);
-					var fieldsConfigElement = 
-						manifestDoc.Elements("format").Elements("regular-grammar").Elements("fields-config").FirstOrDefault();
-					if (fieldsConfigElement == null)
-					{
-						Console.WriteLine("ERROR: failed tofind fields config in '{0}'", formatFile.AbsolutePath);
-						continue;
-					}
-					byte[] asmBytes = precomp.Precompile(LJTraceSource.EmptyTracer);
-					XElement precompiledElement = fieldsConfigElement.Element("precompiled");
-					if (precompiledElement == null)
-					{
-						precompiledElement = new XElement("precompiled");
-						fieldsConfigElement.Add(precompiledElement);
-					}
-					precompiledElement.RemoveAll();
-					precompiledElement.Add(new XCData(Convert.ToBase64String(asmBytes)));
-
-					manifestDoc.Save(formatFile.AbsolutePath);
-
-					Console.WriteLine("Successfully precompiled '{0}'", formatFile.AbsolutePath);
+					HandleFormatFile(tempFormatFilePath, modelObjects);
+					File.Copy(tempFormatFilePath, formatFilePath, overwrite: true);
 				}
 			});
+		}
+
+		private static void HandlePlugin(string pluginPath, ModelObjects modelObjects)
+		{
+			modelObjects.PluginsManager.LoadPlugins(new
+			{
+				Model = modelObjects.ExpensibilityEntryPoint,
+				Presentation = (LogJoint.UI.Presenters.IPresentation)null
+			}, pluginPath, preferTestPluginEntryPoints: true);
+
+			Extensibility.IPluginManifest pluginManifest = modelObjects.PluginsManager.InstalledPlugins.FirstOrDefault();
+			if (pluginManifest == null)
+			{
+				Console.WriteLine("ERROR: Failed to load plugin from '{0}'", pluginPath);
+				return;
+			}
+			foreach (Extensibility.IPluginFile formatFile in pluginManifest.Files.Where(f => f.Type == Extensibility.PluginFileType.FormatDefinition))
+			{
+				string formatFilePath = formatFile.AbsolutePath;
+			}
+		}
+
+		private static void HandleFormatFile(string formatFilePath, ModelObjects modelObjects)
+		{
+			IUserDefinedFactory formatFactory = modelObjects.UserDefinedFormatsManager.Items.FirstOrDefault(
+				factory => factory.Location == formatFilePath);
+			if (formatFactory == null)
+			{
+				Console.WriteLine("ERROR: Failed to load format definition '{0}'", formatFilePath);
+				return;
+			}
+			var precomp = formatFactory as IPrecompilingLogProviderFactory;
+			if (precomp == null)
+			{
+				Console.WriteLine("WARNING: Skipping '{0}' - precompilation is not supported for it", formatFilePath);
+				return;
+			}
+			var formatDoc = XDocument.Load(formatFilePath);
+			var fieldsConfigElement =
+				formatDoc.Elements("format").Elements("regular-grammar").Elements("fields-config").FirstOrDefault();
+			if (fieldsConfigElement == null)
+			{
+				Console.WriteLine("ERROR: failed tofind fields config in '{0}'", formatFilePath);
+				return;
+			}
+			byte[] asmBytes = precomp.Precompile(LJTraceSource.EmptyTracer);
+			XElement precompiledElement = fieldsConfigElement.Element("precompiled");
+			if (precompiledElement == null)
+			{
+				precompiledElement = new XElement("precompiled");
+				fieldsConfigElement.Add(precompiledElement);
+			}
+			precompiledElement.RemoveAll();
+			precompiledElement.Add(new XCData(Convert.ToBase64String(asmBytes)));
+
+			formatDoc.Save(formatFilePath);
+
+			Console.WriteLine("Successfully precompiled '{0}'", formatFilePath);
 		}
 	}
 }
