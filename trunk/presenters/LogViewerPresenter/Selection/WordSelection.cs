a5d3da4255197f3367c966750ab98effa9e712f5
Sergey <sergeysu@google.com>
2022-06-18 00:06:16 +0200

    support IPv6 detection when selecting whole word by double click

--- trunk/presenters/LogViewerPresenter/Selection/WordSelection.cs
+++ trunk/presenters/LogViewerPresenter/Selection/WordSelection.cs
@@ -1,21 +1,17 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
 using LogJoint.RegularExpressions;
-using LogJoint.Search;
 
 namespace LogJoint.UI.Presenters.LogViewer
 {
 	/// <summary>
 	/// Encapsulates word boundaries detection logic for selection in the viewer.
-	/// Meaning of "word" is extended. Whole guids are treated as words.
+	/// Meaning of "word" is extended. For examople, whole guids are treated as words.
 	/// </summary>
 	internal interface IWordSelection
 	{
 		Tuple<int, int> FindWordBoundaries(StringSlice line, int pos);
-		bool IsWordBoundary(StringSlice line, int b, int e);
-		bool IsWord(StringSlice str);
 	};
 
 	internal class WordSelection: IWordSelection
@@ -26,10 +22,16 @@ namespace LogJoint.UI.Presenters.LogViewer
 				@"^[\da-f\-]{1,36}$", ReOptions.IgnoreCase);
 			isGuidRegex = regexFactory.Create(
 				@"^[\da-f]{8}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{4}\-[\da-f]{12}$", ReOptions.IgnoreCase);
-			isPartOfIPRegex = regexFactory.Create(
+			isPartOfIPv4Regex = regexFactory.Create(
 				@"^[\d\.\:]{1,21}$", ReOptions.IgnoreCase);
-			ipRegex = regexFactory.Create(
+			ipIPv4Regex = regexFactory.Create(
 				@"(?<p1>\d{1,3})\.(?<p2>\d{1,3})\.(?<p3>\d{1,3})\.(?<p4>\d{1,3})(\:(?<port>\d+))?", ReOptions.IgnoreCase);
+			isPartOfIPv6Regex = regexFactory.Create(
+				@"^[a-f:\d\[\]]{1,48}$", ReOptions.IgnoreCase);
+			ipIPv6WithPortRegex = regexFactory.Create(
+				@"\[(?<ip>[a-f:\d]+)\]:(?<port>\d+)", ReOptions.IgnoreCase);
+			ipIPv6WithoutPortRegex = regexFactory.Create(
+				@"[a-f:\d]+", ReOptions.IgnoreCase);
 		}
 
 		Tuple<int, int> IWordSelection.FindWordBoundaries(StringSlice line, int pos)
@@ -40,24 +42,13 @@ namespace LogJoint.UI.Presenters.LogViewer
 			int end = line.ZipWithIndex().Skip(pos).Union(new KeyValuePair<int, char>(line.Length, ' ')).FirstOrDefault(isNotAWordChar).Key;
 			if (begin != end)
 			{
-				if (!TryExpandSelectionToCoverGuid(ref begin, ref end, line))
-					TryExpandSelectionToCoverIP(ref begin, ref end, line);
+				bool expanded = false;
+				expanded = expanded || TryExpandSelectionToCoverGuid(ref begin, ref end, line);
+				expanded = expanded || TryExpandSelectionToCoverIPv4(ref begin, ref end, line);
+				expanded = expanded || TryExpandSelectionToCoverIPv6(ref begin, ref end, line);
 				return new Tuple<int, int>(begin, end);
 			}
 			return null;
-		}
-
-		bool IWordSelection.IsWordBoundary(StringSlice line, int b, int e)
-		{
-			return e > b && line.IsWordBoundary(b, e);
-		}
-
-		bool IWordSelection.IsWord(StringSlice str)
-		{
-			return 
-				str.All(StringUtils.IsWordChar) ||
-				isGuidRegex.IsMatch(str) ||
-				IsIP(str);
 		}
 
 		static bool TryExpandSelection(ref int begin, ref int end, StringSlice line, Func<StringSlice, bool, StringSlice?> testSubstr)
@@ -86,39 +77,83 @@ namespace LogJoint.UI.Presenters.LogViewer
 				(partialTest ? isPartOfGuidRegex : isGuidRegex).IsMatch(substr) ? substr : new StringSlice?());
 		}
 
-		bool TryExpandSelectionToCoverIP(ref int begin, ref int end, StringSlice line)
+		bool TryExpandSelectionToCoverIPv4(ref int begin, ref int end, StringSlice line)
 		{
 			return TryExpandSelection(ref begin, ref end, line, (substr, partialTest) =>
 			{
 				if (partialTest)
-					return isPartOfIPRegex.IsMatch(substr) ? substr : new StringSlice?();
-				return FindIP(substr);
+					return isPartOfIPv4Regex.IsMatch(substr) ? substr : new StringSlice?();
+				return FindIPv4(substr);
 			});
 		}
 
-		bool IsIP(StringSlice str)
+		bool TryExpandSelectionToCoverIPv6(ref int begin, ref int end, StringSlice line)
 		{
-			return (FindIP(str) ?? StringSlice.Empty) == str;
+			return TryExpandSelection(ref begin, ref end, line, (substr, partialTest) =>
+			{
+				if (partialTest)
+					return isPartOfIPv6Regex.IsMatch(substr) ? substr : new StringSlice?();
+				return FindIPv6(substr);
+			});
 		}
 
-		StringSlice? FindIP(StringSlice str)
+		StringSlice? FindIPv4(StringSlice str)
 		{
 			IMatch m = null;
-			if (!ipRegex.Match(str.Buffer, str.StartIndex, str.Length, ref m))
+			if (!ipIPv4Regex.Match(str.Buffer, str.StartIndex, str.Length, ref m))
 				return null;
 			for (var i = 1; i <= 4; ++i)
 				if (int.Parse(m.Groups[i].ToString(str.Buffer)) > 255)
 					return null;
 			var port = m.Groups[5].ToString(str.Buffer);
 			if (port.Length > 0)
-				if (int.Parse(port) > 65535)
+				if (!int.TryParse(port, out var portNum) || portNum > 65535)
 					return null;
 			return m.Groups[0].ToStringSlice(str.Buffer);
 		}
 
+		bool IsIPv6WithoutPort(string str)
+		{
+			var parts = str.Split(new[] { ':' }, StringSplitOptions.None);
+			if (parts.Length < 2)
+				return false;
+			if (parts.Length > 8)
+				return false;
+			foreach (var part in parts)
+			{
+				if (part.Length == 0)
+					continue;
+				if (!uint.TryParse(part, System.Globalization.NumberStyles.HexNumber, null, out var partNum) || partNum > 0xffff)
+					return false;
+			}
+			return true;
+		}
+
+		StringSlice? FindIPv6(StringSlice str)
+		{
+			IMatch m = null;
+			if (ipIPv6WithPortRegex.Match(str.Buffer, str.StartIndex, str.Length, ref m))
+			{
+				if (IsIPv6WithoutPort(m.Groups[1].ToString(str.Buffer)) 
+				 && int.TryParse(m.Groups[2].ToString(str.Buffer), out var portNum)
+				 && portNum < 65538)
+					return m.Groups[0].ToStringSlice(str.Buffer);
+			}
+			m = null;
+			if (ipIPv6WithoutPortRegex.Match(str.Buffer, str.StartIndex, str.Length, ref m))
+			{
+				if (IsIPv6WithoutPort(m.Groups[0].ToString(str.Buffer)))
+					return m.Groups[0].ToStringSlice(str.Buffer);
+			}
+			return null;
+		}
+
 		readonly IRegex isPartOfGuidRegex;
 		readonly IRegex isGuidRegex;
-		readonly IRegex isPartOfIPRegex;
-		readonly IRegex ipRegex;
+		readonly IRegex isPartOfIPv4Regex;
+		readonly IRegex ipIPv4Regex;
+		readonly IRegex isPartOfIPv6Regex;
+		readonly IRegex ipIPv6WithPortRegex;
+		readonly IRegex ipIPv6WithoutPortRegex;
 	};
 };
---
+++ trunk/tests/presentation/LogViewerPresenter/WordSelectionTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LogJoint.UI.Presenters.LogViewer;
+
+namespace LogJoint.UI.Presenters.Tests.WordSelectionTests
+{
+	[TestFixture]
+	public class WordSelectionTests
+	{
+		IWordSelection wordSelection = new WordSelection(RegularExpressions.FCLRegexFactory.Instance);
+
+		[Test]
+		public void FindsRegularWord()
+		{
+			Assert.AreEqual(Tuple.Create(0, 4), wordSelection.FindWordBoundaries(new StringSlice("test foo bar"), 0));
+			Assert.AreEqual(Tuple.Create(0, 4), wordSelection.FindWordBoundaries(new StringSlice("test foo bar"), 1));
+			Assert.AreEqual(Tuple.Create(0, 4), wordSelection.FindWordBoundaries(new StringSlice("test foo bar"), 4));
+			Assert.AreEqual(Tuple.Create(5, 8), wordSelection.FindWordBoundaries(new StringSlice("test foo bar"), 5));
+			Assert.AreEqual(Tuple.Create(9, 12), wordSelection.FindWordBoundaries(new StringSlice("test foo bar"), 12));
+		}
+
+		[Test]
+		public void DoesNotFindWordsAmongNonWordChars()
+		{
+			Assert.IsNull(wordSelection.FindWordBoundaries(new StringSlice("-=-~ test"), 2));
+		}
+
+		[Test]
+		public void FindsGuid()
+		{
+			Assert.AreEqual(Tuple.Create(3, 39), wordSelection.FindWordBoundaries(new StringSlice("-= f427e8d3-8779-4a0f-9cfd-735a10a00a0b test"), 4));
+			Assert.AreEqual(Tuple.Create(4, 40), wordSelection.FindWordBoundaries(new StringSlice("-= {f427e8d3-8779-4a0f-9cfd-735a10a00a0b} test"), 4));
+		}
+
+		[Test]
+		public void FindsIPv4()
+		{
+			Assert.AreEqual(Tuple.Create(3, 18), wordSelection.FindWordBoundaries(new StringSlice("-= 192.168.255.255 test"), 4));
+			Assert.AreEqual(Tuple.Create(3, 19), wordSelection.FindWordBoundaries(new StringSlice("-= 10.11.12.13:8000 test"), 4));
+			Assert.AreEqual(Tuple.Create(3, 15), wordSelection.FindWordBoundaries(new StringSlice("-= 127.0.0.1:25 test"), 4));
+
+			Assert.AreEqual(Tuple.Create(3, 6), wordSelection.FindWordBoundaries(new StringSlice("-= 127.0.999.1 test"), 4));
+			Assert.AreEqual(Tuple.Create(3, 6), wordSelection.FindWordBoundaries(new StringSlice("-= 127.0.999999999999999999999999999.1 test"), 4));
+			Assert.AreEqual(Tuple.Create(3, 6), wordSelection.FindWordBoundaries(new StringSlice("-= 127.0.0.1:99999 test"), 4));
+			Assert.AreEqual(Tuple.Create(3, 6), wordSelection.FindWordBoundaries(new StringSlice("-= 127.0.0.1:999999999999999999999999999 test"), 4));
+		}
+
+		[Test]
+		public void FindsIPv6()
+		{
+			Assert.AreEqual(Tuple.Create(3, 42), wordSelection.FindWordBoundaries(new StringSlice("-= 2001:0db8:85a3:0000:0000:8a2e:0370:7334 test"), 4));
+			Assert.AreEqual(Tuple.Create(3, 49), wordSelection.FindWordBoundaries(new StringSlice("-= [2001:0db8:85a3:0000:0000:8a2e:0370:7334]:8080 test"), 4));
+			Assert.AreEqual(Tuple.Create(3, 16), wordSelection.FindWordBoundaries(new StringSlice("-= 2001:db8::1:0 test"), 4));
+			Assert.AreEqual(Tuple.Create(3, 6), wordSelection.FindWordBoundaries(new StringSlice("-= ::1 test"), 6));
+			Assert.AreEqual(Tuple.Create(3, 13), wordSelection.FindWordBoundaries(new StringSlice("-= [::1]:8080 test"), 6));
+		}
+	}
+}
