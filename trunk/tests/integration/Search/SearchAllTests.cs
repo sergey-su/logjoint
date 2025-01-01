0fa408b7a4b4d360eed0772c2875d42323ef5dfa
Sergey <sergeysu@google.com>
2025-01-01 21:17:53 +0100

    Add integration test for searching all occurences

---
+++ trunk/tests/integration/Search/SearchAllTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+
+namespace LogJoint.Tests.Integration
+{
+    [IntegrationTestFixture]
+    class SearchAllTests
+    {
+        [IntegrationTest]
+        public static async Task FindsAllOccurences(TestAppInstance app)
+        {
+            await app.EmulateUrlDragAndDrop(app.Samples.GetSampleAsUri("chrome_debug_1.log"));
+
+            await app.WaitFor(() =>
+            {
+                return !app.PresentationObjects.ViewModels.LoadedMessages.LogViewer.ViewLines.IsEmpty;
+            });
+
+            await app.SynchronizationContext.Invoke(() =>
+            {
+                app.PresentationObjects.MainFormPresenter.ActivateTab(UI.Presenters.MainForm.TabIDs.Search);
+                app.PresentationObjects.ViewModels.SearchPanel.QuickSearchTextBox.OnChangeText("76674c6c636d674962746854");
+                app.PresentationObjects.ViewModels.SearchPanel.QuickSearchTextBox.OnKeyDown(UI.Presenters.QuickSearchTextBox.Key.Enter);
+                Check.That(app.PresentationObjects.ViewModels.SearchResult.IsSearchResultsVisible).IsTrue();
+            });
+
+            await app.WaitFor(() =>
+            {
+                return app.PresentationObjects.ViewModels.SearchResult.LogViewer.ViewLines.Length == 3;
+            });
+
+            await app.SynchronizationContext.Invoke(() =>
+            {
+                List<string> lines = app.PresentationObjects.ViewModels.SearchResult.LogViewer.ViewLines.Select(line => line.TextLineValue).ToList();
+                Check.That(lines[0].Contains("Sending STUN ping, id=76674c6c636d674962746854")).IsTrue();
+                Check.That(lines[1].Contains("Sent STUN ping, id=76674c6c636d674962746854")).IsTrue();
+                Check.That(lines[2].Contains("Received STUN ping response, id=76674c6c636d674962746854")).IsTrue();
+            });
+        }
+    }
+}
